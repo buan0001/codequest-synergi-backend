c7e4030c8acaf75c5ad7a42d812cbdc5406b5101
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-11-28 10:23:46 +0100

    nu med get en side

--- routes/pages.js
+++ routes/pages.js
@@ -8,4 +8,20 @@ pageRouter.get("/", async (req, res) => {
   res.json(test);
 });
 
+pageRouter.get("/:pageTitle", async (req, res) => {
+  const pageTitle = req.params.pageTitle;
+
+  try {
+    const result = await PageModel.findOne({ pageTitle });
+
+    if (!result) {
+      return res.status(404).json({ message: `Data not found for pageTitle: ${pageTitle}` });
+    }
+
+    res.json(result);
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+});
+
 export default pageRouter;
