00282f1b34dc85b925c1ecdf44fd1a90b7f9574e
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-11-28 10:00:45 +0100

    nu med get all pages

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
 import frontpageRouter from "./routes/frontpage.js";
+import pageRouter from "./routes/pages.js";
 // import connection from "database.js";
-
 
 const app = express();
 const port = process.env.PORT || 3333;
@@ -10,8 +10,8 @@ const port = process.env.PORT || 3333;
 app.use(express.json());
 app.use(cors());
 
-
-app.use("/frontpage", frontpageRouter);
+// app.use("/frontpage", frontpageRouter);
+app.use("/pages", pageRouter);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}, http://localhost:${port}`);
